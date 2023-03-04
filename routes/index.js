aaf6ceede64ccbf4524280272d3ee67ec95853b2
wahyunanandika <wahyunanandika@gmail.com>
2023-03-04 21:14:35 +0700

    feat: bikin route untuk linked in login controllernya masuk di customer controllerCustomer semua fuction untuk get token, email sama username sudah berhasil dan masuk ke dalam database find or createnya udah jalan karena .env nya di git ignore jadi ga bisa di apke disini nanti lo cek aja du di jwt secretnya gue hard code || 'test', nanti lo apus aja ya kalo mau masuk production

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const publicRoute = require("./publicRoute")
 const templateRoute = require("./templateRoute")
+const userRoute = require('./usersRoute')
 const express = require('express')
 const ControllerPublic = require("../controllers/public/controllerPublic")
 const router = express.Router()
@@ -8,5 +9,7 @@ router.post('/register', ControllerPublic.adminRegister)
 router.post('/login', ControllerPublic.adminLogin)
 router.use('/public', publicRoute)
 router.use('/templates', templateRoute)
+router.use('/users', userRoute)
+
 
 module.exports = router
