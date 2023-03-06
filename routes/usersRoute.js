40139156ba16da38b50b86147567132d02786560
eduardusoldi <eduardusoldi98@gmail.com>
2023-03-06 20:31:03 +0700

    feat: add nodemailer

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,11 +1,11 @@
-// const express = require('express')
-// const ControllerCustomer = require('../controllers/login-first/controllerCustomer')
-// const userRoute = express.Router()
+const express = require('express')
+const ControllerCustomer = require('../controllers/login-first/controllerCustomer')
+const userRoute = express.Router()
 
-// userRoute.get('/linkedin-request-auth', ControllerCustomer.LinkedinLogin)                   
-// userRoute.post('/linkedin-user-auth', ControllerCustomer.getAuthToken)                   
-// userRoute.post('/me', ControllerCustomer.getMe)
-// userRoute.post('/getemail', ControllerCustomer.getEMail)
-// userRoute.post('/loginOrRegister', ControllerCustomer.linkedinFinalAuth)  
+userRoute.get('/linkedin-request-auth', ControllerCustomer.LinkedinLogin)                   
+userRoute.post('/linkedin-user-auth', ControllerCustomer.getAuthToken)                   
+userRoute.post('/me', ControllerCustomer.getMe)
+userRoute.post('/getemail', ControllerCustomer.getEMail)
+userRoute.post('/loginOrRegister', ControllerCustomer.linkedinFinalAuth)  
 
-// module.exports = userRoute
+module.exports = userRoute
