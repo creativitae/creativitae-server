90cc949edb10b0535a0aec37cb42b3c7bc7b62d7
Reynzzz <Rayhanay115@gmail.com>
2023-03-06 13:09:11 +0700

    nodemailer

--- controllers/public/controllerPublic.js
+++ controllers/public/controllerPublic.js
@@ -1,5 +1,6 @@
 const { compare } = require("../../helpers/bcryptjs");
 const { createToken } = require("../../helpers/jwt");
+const sendEmail = require("../../helpers/nodemailer");
 const {
   Admin,
   Customer,
@@ -55,6 +56,7 @@ class ControllerPublic {
         };
 
       let access_token = createToken({ id: customer.id, email: customer.email });
+        sendEmail(email)
       res
         .status(200)
         .json({ access_token, id: customer.id, username: customer.username, email: customer.email });
