9155541c61cf442b20b145aaccb1964c7a4dcc06
wahyunanandika <wahyunanandika@gmail.com>
2023-03-08 21:51:19 +0700

    kurang authen

--- routes/templateRoute.js
+++ routes/templateRoute.js
@@ -6,7 +6,7 @@ const upload = require('../helpers/multer')
 const { authenticationAdmin, authenticationCustomer } = require('../middlewares/auth') 
 const templateRoute = express.Router()
 
-templateRoute.get('/', ControllerPublic.getTemplate)
+templateRoute.get('/', authenticationCustomer,ControllerPublic.getTemplate)
 templateRoute.post('/', authenticationAdmin, ControllerAdmin.createTemplate)
 templateRoute.post('/uploadCV', ControllerCustomer.base64ToCloud)
 templateRoute.use('/upload-images', authenticationCustomer,upload.array('image'),ControllerCustomer.uploadImage) // buat upload image ke cloudinary, nanti tambahin authen ya di y=taronya sebelum upload.array, ini buat coba jalan doang
